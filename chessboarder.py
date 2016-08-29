c654a3fd11b8585133c6f9f930afb098c92eb53c
gr4viton <lordmutty@gmail.com>
2016-08-29 13:14:35 +0200

    commandline error correction

--- chessboarder.py
+++ chessboarder.py
@@ -50,7 +50,7 @@ class Chessboarder():
         if self.color2:
             col+= self.color2
 
-        self.title = 'chessboard {5}x{6} [wh={0},{1}{4}] [xy-offsets={2},{3}{4}] [{7}]'.format(
+        self.title = 'chessboard {5}x{6} [wh={0},{1}{4}] [{7}] [xy-offsets={2},{3}{4}]'.format(
             self.a, self.b, self.x, self.y, self.unit_str, self.numx, self.numy, col
         )
         print(self.title)
@@ -96,51 +96,7 @@ class Chessboarder():
 
 if __name__ == '__main__':
     help_string = 'chessboarder -a <a-side> -b <b-side> -x <x-offsets> -y <y-offsets> -c <color-odd> -C <color2-even> -X <paper-width> -Y <paper-height> -[t]itle-print -f <frame-print-color>'
-    argv = sys.argv
-
-    try:
-      opts, args = getopt.getopt(argv, 'ha:bxycCtfXY:',
-                                 ['help=', '?=', 'x-offset=', 'y-offset=', 'color=', 'color2=', 'a-side=', 'b-side=', 'paper-width=', 'paper-height=', 'frame-color='])
-    except getopt.GetoptError:
-        print('chessboarder --help')
-
-    if opts:
-        unit_str='mm'
-        title=False
-        color='#000000'
-        color2=None
-        h=297
-        w=210
-        frame_color=None
-
-        for opt, arg in self.opts:
-            if opt in ('-h', '--help', '-?'):
-                print(self.help_string)
-                sys.exit()
-            elif opt in ('-a', '--a-side'):
-                a = arg
-            elif opt in ('-b', '--b-side'):
-                b = arg
-            elif opt in ('-x', '--x-offset'):
-                x = arg
-            elif opt in ('-y', '--y-offset'):
-                y = arg
-            elif opt in ('-c', '--color'):
-                color = arg
-            elif opt in ('-C', '--color2'):
-                color2 = arg
-            elif opt in ('-X', '--paper-width'):
-                w = arg
-            elif opt in ('-Y', '--paper-height'):
-                h = arg
-            elif opt in ('-f', '--frame-color'):
-                frame_color = arg
-            elif opt == '-t':
-                print_title = True
-        Chessboarder(a, b, x, y, color='#000000', color2=color2,
-                     h=297, w=210, frame_color=frame_color, print_title=print_title)
-
-    else:
+    if len(sys.argv) <= 1:
         print(help_string)
         print('Generating test chessboards..')
         print(sys.path[0])
@@ -150,4 +106,72 @@ if __name__ == '__main__':
         Chessboarder(a, b, x, y, color2='#acdc42', frame_color='red', print_title=True)
         a = b = 25
         Chessboarder(a, b, x, y)
+        exit()
+
+    argv = sys.argv[1:]
+    # print(sys.argv)
+    # args = 'chessboarder.py -a -b -cfoo -d bar a1 a2'.split()
+    # print(args)
+    # # ['-a', '-b', '-cfoo', '-d', 'bar', 'a1', 'a2']
+    # optlist, args = getopt.getopt(args, 'abc:d:')
+    # print(optlist)
+    # #[('-a', ''), ('-b', ''), ('-c', 'foo'), ('-d', 'bar')]
+    # print(args)
+    # # ['a1', 'a2']
+
+    try:
+      print('Args to parse:', argv)
+      argOptString = 'ha:b:x:y:c:C:tf:X:Y:'
+      argLongOptString = ['help=', '?=', 'x-offset=', 'y-offset=', 'color=', 'color2=', 'a-side=', 'b-side=', 'paper-width=', 'paper-height=', 'frame-color=']
+      opts, args = getopt.getopt(argv, argOptString, argLongOptString)
+      print('Parsed opts:', opts)
+      print('Not parsed args:', args)
+
+    except getopt.GetoptError:
+        print('chessboarder --help')
+
+    else:
+        if opts:
+            unit_str='mm'
+            title=False
+            color='#000000'
+            color2=None
+            h=297
+            w=210
+            frame_color=None
+            b = None
+            x = y = 10
+            print_title = None
+
+            for opt, arg in opts:
+                if opt in ('-h', '--help', '-?'):
+                    print(self.help_string)
+                    sys.exit()
+                elif opt in ('-a', '--a-side'):
+                    a = float(arg)
+                elif opt in ('-b', '--b-side'):
+                    b = float(arg)
+                elif opt in ('-x', '--x-offset'):
+                    x = float()
+                elif opt in ('-y', '--y-offset'):
+                    y = float(arg)
+                elif opt in ('-c', '--color'):
+                    color = arg
+                elif opt in ('-C', '--color2'):
+                    color2 = arg
+                elif opt in ('-X', '--paper-width'):
+                    w = float(arg)
+                elif opt in ('-Y', '--paper-height'):
+                    h = float(arg)
+                elif opt in ('-f', '--frame-color'):
+                    frame_color = arg
+                elif opt == '-t':
+                    print_title = True
+
+            if b is None:
+                b = a
+            Chessboarder(a, b, x, y, color=color, color2=color2,
+                         h=297, w=210, frame_color=frame_color, print_title=print_title)
+            exit()
+
 
